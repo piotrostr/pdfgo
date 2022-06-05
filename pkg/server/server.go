893476171373461741d42fe59842b00f9f221afb
piotrostr <piotr.ostrowski@aol.com>
2022-06-05 14:50:37 +0200

    endpoint for file uploads works

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,29 +1,61 @@
 package server
 
 import (
-	"fmt"
+	"bytes"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	pdf "github.com/pdfcpu/pdfcpu/pkg/api"
 )
+
+func FilterPdfs(files []*multipart.FileHeader) []io.ReadSeeker {
+	pdfs := []io.ReadSeeker{}
+	for _, file := range files {
+		if filepath.Ext(file.Filename) == ".pdf" {
+			f, err := file.Open()
+			if err != nil {
+				continue
+			}
+			defer f.Close()
+
+			buf := bytes.NewBuffer(nil)
+			_, err = io.Copy(buf, f)
+			if err != nil {
+				continue
+			}
+			rs := bytes.NewReader(buf.Bytes())
+			if err == nil {
+				pdfs = append(pdfs, rs)
+			}
+		}
+	}
+	return pdfs
+}
 
 func SetupRouter() (r *gin.Engine, err error) {
 	gin.SetMode(gin.ReleaseMode)
 
 	r = gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "OK\n")
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "OK\n")
 	})
 
-	r.POST("/", func(c *gin.Context) {
-		form, _ := c.MultipartForm()
+	r.POST("/merge", func(ctx *gin.Context) {
+		form, _ := ctx.MultipartForm()
 		files := form.File["upload[]"]
 
-		for _, file := range files {
-			fmt.Println(file.Filename)
+		pdfs := FilterPdfs(files)
+		if len(pdfs) == 0 {
+			ctx.String(http.StatusBadRequest, "no pdf files uploaded\n")
+			return
 		}
-		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded\n", len(files)))
+		var merged bytes.Buffer
+		pdf.Merge(pdfs, &merged, nil)
+		ctx.Data(http.StatusOK, "application/pdf", merged.Bytes())
 	})
 
 	return
